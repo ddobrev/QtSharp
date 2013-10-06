f51b2b989e1b34bf7778831906740f6d5f46b83e
Dimitar Dobrev <dpldobrev@yahoo.com>
2013-10-06 21:53:48 +0300

    Removed static methods from derived types (which is in turn caused by the expansion of the Q_OBJECT macro).
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

---
+++ QtSharp/RemoveStaticsFromDerivedTypesPass.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CppSharp.AST;
+using CppSharp.Passes;
+
+namespace QtSharp
+{
+    public class RemoveStaticsFromDerivedTypesPass : TranslationUnitPass
+    {
+        public override bool VisitClassDecl(Class @class)
+        {
+            if (AlreadyVisited(@class) || @class.Name == "QObject")
+            {
+                return false;
+            }
+
+            IEnumerable<MacroExpansion> expansions = @class.PreprocessedEntities.OfType<MacroExpansion>();
+
+            bool isQObject = expansions.Any(e => e.Text == "Q_OBJECT");
+            if (isQObject)
+            {
+                RemoveQObjectMembers(@class);
+            }
+            return true;
+        }
+
+        private static void RemoveQObjectMembers(Class @class)
+        {
+            // Every Qt object "inherits" a lot of members via the Q_OBJECT macro.
+            // See the define of Q_OBJECT in qobjectdefs.h for a list of the members.
+            // We cannot use the Qt defines for disabling the expansion of these
+            // because it would mess up with the object layout size.
+
+            RemoveMethodOverloads(@class, "metaObject");
+            RemoveMethodOverloads(@class, "qt_metacast");
+            RemoveMethodOverloads(@class, "tr");
+            RemoveMethodOverloads(@class, "trUtf8");
+            RemoveMethodOverloads(@class, "qt_metacall");
+            RemoveMethodOverloads(@class, "qt_static_metacall");
+            RemoveVariables(@class, "staticMetaObject");
+        }
+
+        private static void RemoveMethodOverloads(Class @class, string originalName)
+        {
+            var overloads = @class.FindMethodByOriginalName(originalName).ToList();
+            foreach (var method in overloads)
+                @class.Methods.Remove(method);
+        }
+
+        private static void RemoveVariables(Class @class, string originalName)
+        {
+            var variables = @class.FindVariableByOriginalName(originalName).ToList();
+            foreach (var variable in variables)
+                variable.ExplicityIgnored = true;
+        }
+    }
+}
