c5129574b08f07aebe653ed6ee6101368664f86b
Dimitar Dobrev <dpldobrev@yahoo.com>
2013-11-12 13:02:20 +0200

    Mapped QFlags<T> to T (the underlying enum).
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

---
+++ QtSharp/QFlags.cs
@@ -0,0 +1,27 @@
+﻿using CppSharp.AST;
+using CppSharp.Generators;
+using CppSharp.Generators.CSharp;
+using CppSharp.Types;
+
+namespace QtSharp
+{
+    [TypeMap("QFlags")]
+    public class QFlags : TypeMap
+    {
+        public override string CSharpSignature(CSharpTypePrinterContext ctx)
+        {
+            TemplateArgument templateArgument = ((TemplateSpecializationType) ctx.Type.Desugar()).Arguments[0];
+            return templateArgument.Type.Type.ToString();
+        }
+
+        public override void CSharpMarshalToNative(MarshalContext ctx)
+        {
+            ctx.Return.Write(ctx.Parameter.Name);
+        }
+
+        public override void CSharpMarshalToManaged(MarshalContext ctx)
+        {
+            ctx.Return.Write(ctx.ReturnVarName);
+        }
+    }
+}
