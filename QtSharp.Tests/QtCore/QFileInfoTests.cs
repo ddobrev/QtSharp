5954a20fec51447ec7793bebd91adce90fcf0670
KeKl <mail@kevinklein.de>
2014-09-13 10:24:24 +0200

    Add Tests.

---
+++ QtSharp.Tests/QtCore/QFileInfoTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using QtCore;
+
+namespace QtSharp.Tests.QtCore
+{
+    [TestFixture]
+    public class QFileInfoTests
+    {
+        private readonly string _testFilePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+
+        private QFileInfo _fileInfo;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            // TODO: Add Init code.
+            _fileInfo = new QFileInfo(_testFilePath);
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+            // TODO: Add tear down code.
+        }
+
+        [Test]
+        public void GetAbsoluteFilePathAndTestWithDotNetAbsoluteFilePath()
+        {
+            var q = _fileInfo.AbsoluteFilePath;
+            var exp = _testFilePath.Replace("\\", "/");
+
+            Assert.AreEqual(exp, q);
+        }
+
+        [Test]
+        public void GetFileNameAndTestWithDotNetFileName()
+        {
+            var q = _fileInfo.FileName;
+            var exp = new FileInfo(_testFilePath).Name;
+
+            Assert.AreEqual(exp, q);
+        }
+    }
+}
---
+++ QtSharp.Tests/QtCore/QStringRefTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using QtCore;
+
+namespace QtSharp.Tests.QtCore
+{
+    [TestFixture]
+    public class QStringRefTests
+    {
+        private readonly string _testString = System.Reflection.Assembly.GetCallingAssembly().Location;
+
+        private QStringRef _qString;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            // TODO: Add Init code.
+            _qString = new QStringRef(_testString);
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+            // TODO: Add tear down code.
+        }
+
+        [Test]
+        public void TestQStringToStringAndDotNetStringIfEqual()
+        {
+            var q = _qString.ToString();
+            var exp = this._testString;
+
+            Assert.AreEqual(exp, q);
+        }
+    }
+}
