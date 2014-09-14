11662d5ce2e808c0c4a52dc4aa7e5e0726a27b86
KeKl <mail@kevinklein.de>
2014-09-14 23:05:52 +0200

    Added some QCoreApplication tests.

--- QtSharp.Tests/Helper.cs
+++ QtSharp.Tests/Helper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+using QtCore;
 
 namespace QtSharp.Tests
 {
@@ -22,6 +26,46 @@ namespace QtSharp.Tests
                 builder.Append(ch);
             }
             return builder.ToString();
+        }
+
+        public static string[] GenerateQtArgv(string[] args)
+        {
+            var argv = new string[args.Length + 1];
+
+            var a = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+
+            var attrs = a.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+            if (attrs.Length > 0)
+            {
+                argv[0] = ((AssemblyTitleAttribute)attrs[0]).Title;
+            }
+            else
+            {
+                var info = new QFileInfo(a.Location);
+                argv[0] = info.BaseName;
+            }
+            args.CopyTo(argv, 1);
+
+            return argv;
+        }
+
+        public unsafe static QCoreApplication CreateQCoreApplicationInstance(string[] args)
+        {
+            var argv = GenerateQtArgv(args);
+            var argc = argv.Count();
+
+            var arguments = argv.Aggregate("", (current, arg) => current + (arg + "\0"));
+
+            var stringPointer = (char*)Marshal.StringToHGlobalAuto(arguments).ToPointer();
+
+            Marshal.FreeHGlobal(new IntPtr(stringPointer));
+
+            var app = new QCoreApplication(&argc, null);
+            //var app = new QCoreApplication(&argc, &stringPointer);
+
+            Marshal.FreeHGlobal(new IntPtr(stringPointer));
+
+            return app;
         }
     }
 }
---
+++ QtSharp.Tests/QtCore/QCoreApplicationTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using QtCore;
+
+namespace QtSharp.Tests.QtCore
+{
+    [TestFixture]
+    public class QCoreApplicationTests
+    {
+        private QCoreApplication _qCoreApp;
+
+        [SetUp]
+        public void Init()
+        {
+            // TODO: Add Init code.
+            _qCoreApp = Helper.CreateQCoreApplicationInstance(new[] { "" });
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            // TODO: Add tear down code.
+            QCoreApplication.Exec();
+        }
+
+        [Test]
+        public void TestApplicationName()
+        {
+            var n = QCoreApplication.ApplicationName;
+
+            Assert.IsNotNull(n);
+        }
+
+        [Test]
+        public void TestApplicationVersion()
+        {
+            var n = QCoreApplication.ApplicationVersion;
+
+            Assert.IsNotNull(n);
+        }
+
+        [Test]
+        public void TestOrganizationDomain()
+        {
+            var n = QCoreApplication.OrganizationDomain;
+
+            Assert.IsNotNull(n);
+        }
+
+        [Test]
+        public void TestOrganizationName()
+        {
+            var n = QCoreApplication.OrganizationName;
+
+            Assert.IsNotNull(n);
+        }
+
+        [Test]
+        public void TestQuitLockEnabled()
+        {
+            var n = QCoreApplication.QuitLockEnabled;
+
+            Assert.IsNotNull(n);
+        }
+
+        [Test]
+        public void TestAboutToQuit()
+        {
+            QCoreApplication.Quit();
+
+            var wasFired = false;
+            _qCoreApp.AboutToQuit += () => wasFired = true;
+
+            Assert.IsTrue(wasFired);
+        }
+    }
+}
