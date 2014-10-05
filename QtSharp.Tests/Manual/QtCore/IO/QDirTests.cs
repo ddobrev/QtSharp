e666d98536b311df9f8f51e95201e868f898fb31
KeKl <mail@kevinklein.de>
2014-10-05 12:14:59 +0200

    Added QAbstractEventDispatcher tests.

---
+++ QtSharp.Tests/Manual/QtCore/IO/QDirTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace QtSharp.Tests.Manual.QtCore.IO
+{
+    [TestFixture]
+    public class QDirTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            // TODO: Add Init code.
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            // TODO: Add tear down code.
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
