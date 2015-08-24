52e5696c8926dee03186d4847c74e5b5933c74b0
Dimitar Dobrev <dpldobrev@yahoo.com>
2015-08-24 16:03:27 +0300

    Properly aligned the code in QtSharp.cs.
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

--- QtSharp/QtSharp.cs
+++ QtSharp/QtSharp.cs
@@ -13,46 +13,47 @@ using CppAbi = CppSharp.Parser.AST.CppAbi;
 
 namespace QtSharp
 {
-	public class QtSharp : ILibrary
-	{
-	    private readonly string qmake;
-	    private readonly string make;
-	    private readonly string includePath;
-	    private readonly string module;
-	    private readonly string libraryPath;
-	    private readonly string library;
-	    private readonly IEnumerable<string> systemIncludeDirs;
-	    private readonly string target;
-	    private readonly string docs;
+    public class QtSharp : ILibrary
+    {
+        private readonly string qmake;
+        private readonly string make;
+        private readonly string includePath;
+        private readonly string module;
+        private readonly string libraryPath;
+        private readonly string library;
+        private readonly IEnumerable<string> systemIncludeDirs;
+        private readonly string target;
+        private readonly string docs;
 
-	    public QtSharp(string qmake, string make, string includePath, string libraryPath, string library, string target, IEnumerable<string> systemIncludeDirs, string docs)
-	    {
-	        this.qmake = qmake;
-	        this.includePath = includePath.Replace('/', Path.DirectorySeparatorChar);
-	        this.module = Regex.Match(library, @"Qt\d?(?<module>\w+)\.\w+$").Groups["module"].Value;
-	        this.libraryPath = libraryPath.Replace('/', Path.DirectorySeparatorChar);
-	        this.library = library;
-	        this.target = target;
-	        this.systemIncludeDirs = systemIncludeDirs;
-	        this.make = make;
+        public QtSharp(string qmake, string make, string includePath, string libraryPath, string library, string target,
+            IEnumerable<string> systemIncludeDirs, string docs)
+        {
+            this.qmake = qmake;
+            this.includePath = includePath.Replace('/', Path.DirectorySeparatorChar);
+            this.module = Regex.Match(library, @"Qt\d?(?<module>\w+)\.\w+$").Groups["module"].Value;
+            this.libraryPath = libraryPath.Replace('/', Path.DirectorySeparatorChar);
+            this.library = library;
+            this.target = target;
+            this.systemIncludeDirs = systemIncludeDirs;
+            this.make = make;
             this.docs = docs;
-	    }
+        }
 
-	    public void Preprocess(Driver driver, ASTContext lib)
-	    {
+        public void Preprocess(Driver driver, ASTContext lib)
+        {
             var qtModule = "Qt" + this.module;
-	        var moduleIncludes = Path.Combine(this.includePath, qtModule);
-	        foreach (var unit in lib.TranslationUnits.Where(u => u.FilePath != "<invalid>"))
-	        {
-	            if (Path.GetDirectoryName(unit.FilePath) != moduleIncludes)
-	            {
+            var moduleIncludes = Path.Combine(this.includePath, qtModule);
+            foreach (var unit in lib.TranslationUnits.Where(u => u.FilePath != "<invalid>"))
+            {
+                if (Path.GetDirectoryName(unit.FilePath) != moduleIncludes)
+                {
                     LinkDeclaration(unit);
-	            }
-	            else
-	            {
-	                IgnorePrivateDeclarations(unit);
-	            }
-	        }
+                }
+                else
+                {
+                    IgnorePrivateDeclarations(unit);
+                }
+            }
             lib.SetClassAsValueType("QByteArray");
             lib.SetClassAsValueType("QListData");
             lib.SetClassAsValueType("QListData::Data");
@@ -69,25 +70,25 @@ namespace QtSharp
             lib.SetClassAsValueType("QVariant");
             lib.IgnoreClassMethodWithName("QString", "fromStdWString");
             lib.IgnoreClassMethodWithName("QString", "toStdWString");
-	        if (this.module == "Widgets")
-	        {
-	            string[] classesWithTypeEnums =
-                    {
-                        "QGraphicsEllipseItem", "QGraphicsItemGroup", "QGraphicsLineItem",
-                        "QGraphicsPathItem", "QGraphicsPixmapItem", "QGraphicsPolygonItem", "QGraphicsProxyWidget",
-                        "QGraphicsRectItem", "QGraphicsSimpleTextItem", "QGraphicsTextItem", "QGraphicsWidget"
-                    };
-	            foreach (var enumeration in classesWithTypeEnums.Select(c => lib.FindCompleteClass(c)).SelectMany(
-                                                @class => @class.Enums.Where(e => string.IsNullOrEmpty(e.Name))))
-	            {
-	                enumeration.Name = "TypeEnum";
-	            }
-	        }
-	    }
+            if (this.module == "Widgets")
+            {
+                string[] classesWithTypeEnums =
+                {
+                    "QGraphicsEllipseItem", "QGraphicsItemGroup", "QGraphicsLineItem",
+                    "QGraphicsPathItem", "QGraphicsPixmapItem", "QGraphicsPolygonItem", "QGraphicsProxyWidget",
+                    "QGraphicsRectItem", "QGraphicsSimpleTextItem", "QGraphicsTextItem", "QGraphicsWidget"
+                };
+                foreach (var enumeration in classesWithTypeEnums.Select(c => lib.FindCompleteClass(c)).SelectMany(
+                    @class => @class.Enums.Where(e => string.IsNullOrEmpty(e.Name))))
+                {
+                    enumeration.Name = "TypeEnum";
+                }
+            }
+        }
 
-	    private static void LinkDeclaration(Declaration declaration)
-	    {
-	        declaration.GenerationKind = GenerationKind.Link;
+        private static void LinkDeclaration(Declaration declaration)
+        {
+            declaration.GenerationKind = GenerationKind.Link;
             DeclarationContext declarationContext = declaration as DeclarationContext;
             if (declarationContext != null)
             {
@@ -96,39 +97,40 @@ namespace QtSharp
                     LinkDeclaration(nestedDeclaration);
                 }
             }
-	    }
+        }
 
-	    private static void IgnorePrivateDeclarations(DeclarationContext unit)
-	    {
-	        foreach (var declaration in unit.Declarations)
-	        {
-	            IgnorePrivateDeclaration(declaration);
-	        }
-	    }
+        private static void IgnorePrivateDeclarations(DeclarationContext unit)
+        {
+            foreach (var declaration in unit.Declarations)
+            {
+                IgnorePrivateDeclaration(declaration);
+            }
+        }
 
-	    private static void IgnorePrivateDeclaration(Declaration declaration)
-	    {
-	        if (declaration.Name != null &&
-	            (declaration.Name.StartsWith("Private") || declaration.Name.EndsWith("Private")))
-	        {
-	            declaration.ExplicityIgnored = true;
-	        }
-	        else
-	        {
-	            DeclarationContext declarationContext = declaration as DeclarationContext;
-	            if (declarationContext != null)
-	            {
-	                IgnorePrivateDeclarations(declarationContext);
-	            }
-	        }
-	    }
+        private static void IgnorePrivateDeclaration(Declaration declaration)
+        {
+            if (declaration.Name != null &&
+                (declaration.Name.StartsWith("Private") || declaration.Name.EndsWith("Private")))
+            {
+                declaration.ExplicityIgnored = true;
+            }
+            else
+            {
+                DeclarationContext declarationContext = declaration as DeclarationContext;
+                if (declarationContext != null)
+                {
+                    IgnorePrivateDeclarations(declarationContext);
+                }
+            }
+        }
 
         public void Postprocess(Driver driver, ASTContext lib)
         {
             new ClearCommentsPass().VisitLibrary(driver.ASTContext);
             new GetCommentsFromQtDocsPass(this.docs, this.module).VisitLibrary(driver.ASTContext);
             new CaseRenamePass(
-                RenameTargets.Function | RenameTargets.Method | RenameTargets.Property | RenameTargets.Delegate | RenameTargets.Field | RenameTargets.Variable,
+                RenameTargets.Function | RenameTargets.Method | RenameTargets.Property | RenameTargets.Delegate |
+                RenameTargets.Field | RenameTargets.Variable,
                 RenameCasePattern.UpperCamelCase).VisitLibrary(driver.ASTContext);
             switch (this.module)
             {
@@ -144,63 +146,63 @@ namespace QtSharp
             }
         }
 
-		public void Setup(Driver driver)
-		{
-			driver.Options.GeneratorKind = GeneratorKind.CSharp;
-		    var qtModule = "Qt" + this.module;
+        public void Setup(Driver driver)
+        {
+            driver.Options.GeneratorKind = GeneratorKind.CSharp;
+            var qtModule = "Qt" + this.module;
             // HACK: work around https://bugreports.qt.io/browse/QTBUG-47569
-		    if (this.module == "Widgets")
-		    {
+            if (this.module == "Widgets")
+            {
                 driver.Options.addDefines("QT_NO_ACCESSIBILITY");
-		    }
-		    driver.Options.MicrosoftMode = false;
-		    driver.Options.NoBuiltinIncludes = true;
+            }
+            driver.Options.MicrosoftMode = false;
+            driver.Options.NoBuiltinIncludes = true;
             driver.Options.TargetTriple = this.target;
             driver.Options.Abi = CppAbi.Itanium;
-		    driver.Options.LibraryName = string.Format("{0}Sharp", qtModule);
-		    driver.Options.OutputNamespace = qtModule;
-			driver.Options.Verbose = true;
-		    driver.Options.GenerateInterfacesForMultipleInheritance = true;
+            driver.Options.LibraryName = string.Format("{0}Sharp", qtModule);
+            driver.Options.OutputNamespace = qtModule;
+            driver.Options.Verbose = true;
+            driver.Options.GenerateInterfacesForMultipleInheritance = true;
             driver.Options.GeneratePropertiesAdvanced = true;
-			driver.Options.IgnoreParseWarnings = true;
-		    driver.Options.CheckSymbols = true;
+            driver.Options.IgnoreParseWarnings = true;
+            driver.Options.CheckSymbols = true;
             driver.Options.GenerateSingleCSharpFile = true;
-		    driver.Options.GenerateInlines = true;
-		    driver.Options.CompileCode = true;
-		    driver.Options.GenerateCopyConstructors = true;
-		    driver.Options.GenerateDefaultValuesForArguments = true;
-		    driver.Options.GenerateConversionOperators = true;
-		    driver.Options.MarshalCharAsManagedChar = true;
+            driver.Options.GenerateInlines = true;
+            driver.Options.CompileCode = true;
+            driver.Options.GenerateCopyConstructors = true;
+            driver.Options.GenerateDefaultValuesForArguments = true;
+            driver.Options.GenerateConversionOperators = true;
+            driver.Options.MarshalCharAsManagedChar = true;
             driver.Options.Headers.Add(qtModule);
-		    foreach (var systemIncludeDir in this.systemIncludeDirs)
-		    {
-		        driver.Options.addSystemIncludeDirs(systemIncludeDir);
-		    }
+            foreach (var systemIncludeDir in this.systemIncludeDirs)
+            {
+                driver.Options.addSystemIncludeDirs(systemIncludeDir);
+            }
             driver.Options.addIncludeDirs(this.includePath);
             driver.Options.addIncludeDirs(Path.Combine(this.includePath, qtModule));
             driver.Options.addLibraryDirs(this.libraryPath);
             driver.Options.Libraries.Add(this.library);
             string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-		    switch (this.module)
-		    {
-		        case "Core":
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "QEventArgs.cs"));
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "QEventHandler.cs"));
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "QObject.cs"));
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "QChar.cs"));
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "_iobuf.cs"));
-		            break;
-		        case "Widgets":
-		            driver.Options.CodeFiles.Add(Path.Combine(dir, "QSceneEventHandler.cs"));
-		            break;
-		    }
-		}
+            switch (this.module)
+            {
+                case "Core":
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "QEventArgs.cs"));
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "QEventHandler.cs"));
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "QObject.cs"));
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "QChar.cs"));
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "_iobuf.cs"));
+                    break;
+                case "Widgets":
+                    driver.Options.CodeFiles.Add(Path.Combine(dir, "QSceneEventHandler.cs"));
+                    break;
+            }
+        }
 
-		public void SetupPasses(Driver driver)
-		{
+        public void SetupPasses(Driver driver)
+        {
             driver.TranslationUnitPasses.AddPass(new CompileInlinesPass(this.qmake, this.make));
             driver.TranslationUnitPasses.AddPass(new GenerateEventEventsPass());
             driver.TranslationUnitPasses.AddPass(new GenerateSignalEventsPass());
-		}
-	}
+        }
+    }
 }
