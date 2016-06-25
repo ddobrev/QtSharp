45381876787ea84669b9090fdb2810a61ddb52cb
Dimitar Dobrev <dpldobrev@protonmail.com>
2016-06-25 03:44:21 +0300

    Fixed the marshalling of enums in signals.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- QtSharp/QObject.cs
+++ QtSharp/QObject.cs
@@ -54,34 +54,65 @@ namespace QtCore
             {
                 System.Reflection.ParameterInfo parameter = @params[i];
                 var arg = new IntPtr(arguments[1 + i]);
-                object value;
-                if (parameter.ParameterType.IsValueType)
-                {
-                    value = Marshal.PtrToStructure(arg, parameter.ParameterType);
-                }
-                else
-                {
-                    if (parameter.ParameterType.IsAssignableFrom(typeof(string)))
-                    {
-                        var metaMethod = this.Sender.MetaObject.Method(handler.SignalId);
-                        if (metaMethod.ParameterType(i) == (int) QMetaType.Type.QString)
-                        {
-                            value = Marshal.PtrToStringUni(new IntPtr(QtCore.QString.__CreateInstance(arg).Utf16));
-                        }
-                        else
-                        {
-                            value = Marshal.PtrToStringUni(arg);
-                        }
-                    }
-                    else
-                    {
-                        value = Activator.CreateInstance(parameter.ParameterType, arg);
-                    }
-                }
-                parameters[i] = value;
+                parameters[i] = GetParameterValue(handler, i, parameter, arg);
             }
             handler.Delegate.DynamicInvoke(parameters);
             return -1;
+        }
+
+        private object GetParameterValue(Handler handler, int i, System.Reflection.ParameterInfo parameter, IntPtr arg)
+        {
+            if (arg == IntPtr.Zero)
+            {
+                return null;
+            }
+            var type = parameter.ParameterType.IsEnum ? parameter.ParameterType.GetEnumUnderlyingType() : parameter.ParameterType;
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Empty:
+                    return null;
+                case TypeCode.Object:
+                    return Activator.CreateInstance(type, arg);
+                case TypeCode.DBNull:
+                    return DBNull.Value;
+                case TypeCode.Boolean:
+                    return *(bool*) arg;
+                case TypeCode.Char:
+                    return *(char*) arg;
+                case TypeCode.SByte:
+                    return *(sbyte*) arg;
+                case TypeCode.Byte:
+                    return *(byte*) arg;
+                case TypeCode.Int16:
+                    return *(short*) arg;
+                case TypeCode.UInt16:
+                    return *(ushort*) arg;
+                case TypeCode.Int32:
+                    return *(int*) arg;
+                case TypeCode.UInt32:
+                    return *(uint*) arg;
+                case TypeCode.Int64:
+                    return *(long*) arg;
+                case TypeCode.UInt64:
+                    return *(ulong*) arg;
+                case TypeCode.Single:
+                    return *(float*) arg;
+                case TypeCode.Double:
+                    return *(double*) arg;
+                case TypeCode.Decimal:
+                    return *(decimal*) arg;
+                case TypeCode.DateTime:
+                    return *(DateTime*) arg;
+                case TypeCode.String:
+                    var metaMethod = this.Sender.MetaObject.Method(handler.SignalId);
+                    if (metaMethod.ParameterType(i) == (int) QMetaType.Type.QString)
+                    {
+                        return Marshal.PtrToStringUni(new IntPtr(QtCore.QString.__CreateInstance(arg).Utf16));
+                    }
+                    return Marshal.PtrToStringUni(arg);
+                default:
+                    throw new ArgumentOutOfRangeException(parameter.Name, "Parameter type with invalid type code.");
+            }
         }
     }
 }
